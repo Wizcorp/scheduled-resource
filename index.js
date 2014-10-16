771565d68de41515336faf0aec084d0c202bd0e6
Bjorn Stromberg <bstromberg@wizcorp.jp>
2014-10-16 19:05:46 +0900

    v0.1.0: First commit

---
+++ index.js
@@ -0,0 +1,105 @@
+var moment = require('moment');
+
+var ONE_HOUR = 60 * 60 * 1000;
+var TIMEZONE_OFFSET = -540;
+
+function priorityReducer(previousValue, currentValue) {
+	return !previousValue ||
+			!previousValue.hasOwnProperty('priority') ||
+			previousValue.priority < currentValue.priority ? currentValue : previousValue;
+}
+
+function ScheduledResource(resource, schedule, now) {
+	now = now || function () { return Date.now(); };
+
+	var cache = {
+		slots: {},
+		validAfter: 0,
+		validUntil: 0
+	};
+
+	function isEntryActive(entry, scheduleTime, timestamp) {
+		var activeAfter = Math.max(entry.start || 0, scheduleTime) * 1000;
+		var activeUntil = Math.min(entry.end || Infinity, scheduleTime + (entry.duration || 1) * 60 * 60) * 1000;
+
+		return timestamp >= activeAfter && timestamp < activeUntil;
+	}
+
+	function getActive(timestamp) {
+		timestamp = timestamp || now();
+
+		if (timestamp >= cache.validAfter && timestamp < cache.validUntil) {
+			return cache.slots;
+		}
+
+		cache.slots = {};
+		cache.validAfter = timestamp - timestamp % ONE_HOUR;
+		cache.validUntil = cache.validAfter + ONE_HOUR;
+
+		var currentTime = moment(timestamp).zone(TIMEZONE_OFFSET);
+
+		var week = currentTime.format('WW');
+
+		for (var dayId in schedule) {
+			var day = dayId === '*' ? currentTime.format('E') : dayId;
+
+			for (var hourId in schedule[dayId]) {
+				var hour = hourId === '*' ? currentTime.format('HH') : hourId;
+
+				var scheduleTime = moment(week + hour, 'WWHH').add(day - 1, 'days').unix();
+
+				for (var slotId in schedule[dayId][hourId]) {
+					if (!cache.slots[slotId]) {
+						cache.slots[slotId] = [];
+					}
+
+					var entries = schedule[dayId][hourId][slotId];
+
+					for (var i = 0; i < entries.length; i += 1) {
+						var entry = entries[i];
+
+						if (isEntryActive(entry, scheduleTime, timestamp)) {
+							cache.slots[slotId].push({ resource: resource[entry.resourceId], priority: entry.priority });
+						}
+					}
+				}
+			}
+		}
+
+		return cache.slots;
+	}
+
+	this.get = function (slotId, timestamp) {
+		var slots = getActive(timestamp);
+
+		if (slotId) {
+			return slots[slotId].reduce(priorityReducer).resource;
+		}
+
+		return slots;
+	};
+
+	this.getList = function (timestamp) {
+		var slots = getActive(timestamp);
+
+		var out = [];
+
+		var slotIds = Object.keys(slots);
+
+		for (var i = 0; i < slotIds.length; i += 1) {
+			var slotId = slotIds[i];
+			if (slots[slotId].length) {
+				out.push(slots[slotId].reduce(priorityReducer).resource);
+			}
+		}
+
+		return out;
+	};
+
+	this.getNextUpdate = function (timestamp) {
+		timestamp = timestamp || now();
+		return timestamp + ONE_HOUR - timestamp % ONE_HOUR;
+	};
+}
+
+module.exports = ScheduledResource;
